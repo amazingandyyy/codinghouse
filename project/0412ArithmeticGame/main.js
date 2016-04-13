8a5a343d82bb89b89a160f51d384182adf93471d
amazingandyyy <qq830214a@gmail.com>
2016-04-13 09:19:34 -0700

    add key input
    add key input

--- project/0412ArithmeticGame/main.js
+++ project/0412ArithmeticGame/main.js
@@ -18,17 +18,18 @@ function init() {
     var clearBtn = document.getElementById('clearBtn');
     var btnSkip = document.getElementById('btnSkip');
     var btnR = document.getElementById('btnR');
-    var answerArr;
+    var answerArr = [];
+    var userAnswer = parseInt(answerArr.join(''));
 
 
 
     firstNum.textContent = Math.floor(Math.random() * 100);
     if (Math.floor(Math.random() < 0.33333333)) {
         relation.textContent = "+";
-    } else if(Math.floor(Math.random() > 0.6666666)) {
+    } else if (Math.floor(Math.random() > 0.6666666)) {
         relation.textContent = "-";
-    }else{
-      relation.textContent = "*";
+    } else {
+        relation.textContent = "*";
     }
     secondNum.textContent = Math.floor(Math.random() * 100);
 
@@ -38,20 +39,20 @@ function init() {
     btnSkip.addEventListener('click', btnSkipClicked);
     // btnR.addEventListener('click', btnRClicked);
 
-
-
-
 }
+
 var answerArr = [];
 
 function numBtnClicked(event) {
-    if (event.target.textContent == "C") {} else if (event.target.textContent == "+/-") {
+    if (event.target.textContent == "C") {
+
+    } else if (event.target.textContent == "+/-") {
         btnRClicked();
         userAnswer = (answerArr.join(''));
         answer.textContent = userAnswer;
     } else {
         answerArr.push(event.target.textContent);
-        var userAnswer = parseInt(answerArr.join(''));
+        userAnswer = parseInt(answerArr.join(''));
         answer.textContent = userAnswer;
     }
 
@@ -59,6 +60,7 @@ function numBtnClicked(event) {
 }
 
 function btnRClicked() {
+    console.log('do -')
     var userAnswer = parseInt(answerArr.join(''));
     if (answerArr.length > 0) {
         if (Math.sign(userAnswer) == 1) {
@@ -69,10 +71,11 @@ function btnRClicked() {
     } else if (answerArr.length == 0) {
         answerArr.push('-');
         userAnswer = answerArr.join('');
-        console.log(answerArr)
-        console.log(userAnswer)
+        // console.log(answerArr)
+        // console.log(userAnswer)
     }
-
+    userAnswer = (answerArr.join(''));
+    answer.textContent = userAnswer;
 }
 
 
@@ -85,22 +88,22 @@ function resultBtnClicked() {
     var userAnswer = parseInt(answerArr.join(''));
     if (relation.textContent == '+') {
         var correctAnswer = parseInt(firstNum.textContent) + parseInt(secondNum.textContent);
-    } else if(relation.textContent == '-') {
+    } else if (relation.textContent == '-') {
         var correctAnswer = parseInt(firstNum.textContent) - parseInt(secondNum.textContent);
-    }else if(relation.textContent == '*'){
-var correctAnswer = parseInt(firstNum.textContent) * parseInt(secondNum.textContent);
+    } else if (relation.textContent == '*') {
+        var correctAnswer = parseInt(firstNum.textContent) * parseInt(secondNum.textContent);
     }
     if (userAnswer == correctAnswer) {
-      window.setTimeout(function(){
-        document.getElementById('win').classList.add('active');
-      }, 600);
-      document.getElementById('whiteCard').classList.add('active');
+        window.setTimeout(function() {
+            document.getElementById('win').classList.add('active');
+        }, 600);
+        document.getElementById('whiteCard').classList.add('active');
         scoresScore += 1;
     } else if (userAnswer !== correctAnswer) {
-      window.setTimeout(function(){
-        document.getElementById('lost').classList.add('active');
-      }, 600);
-      document.getElementById('whiteCard').classList.add('active');
+        window.setTimeout(function() {
+            document.getElementById('lost').classList.add('active');
+        }, 600);
+        document.getElementById('whiteCard').classList.add('active');
         document.getElementById('lost').textContent = "Fail :( It Should be " + correctAnswer + ".";
         scoresScoreF += 1;
     }
@@ -127,14 +130,57 @@ function btnSkipClicked() {
     document.getElementById('lost').classList.add('active');
     document.getElementById('whiteCard').classList.add('active');
     document.getElementById('lost').textContent = "You just skip it.";
-function fun(){
-  document.getElementById('win').classList.remove('active');
-  document.getElementById('lost').classList.remove('active');
-  document.getElementById('whiteCard').classList.remove('active');
-}
+
+    function fun() {
+        document.getElementById('win').classList.remove('active');
+        document.getElementById('lost').classList.remove('active');
+        document.getElementById('whiteCard').classList.remove('active');
+    }
     window.setTimeout(fun, 800);
 
     clearBtnClicked();
     scoresScoreF += 0;
     init();
 }
+
+window.addEventListener("keydown", function(event) {
+    if (event.defaultPrevented) {
+        return; // Should do nothing if the default action has been cancelled
+    }
+
+    var handled = false;
+    if (event.key !== undefined) {
+        // Handle the event with KeyboardEvent.key and set handled true.
+    } else if (event.keyIdentifier !== undefined) {
+        // Handle the event with KeyboardEvent.keyIdentifier and set handled true.
+    } else if (event.keyCode !== undefined) {
+        // Handle the event with KeyboardEvent.keyCode and set handled true.
+    }
+
+    if (handled) {
+        // Suppress "double action" if event handled
+        event.preventDefault();
+    }
+    if (event.keyCode == 189) {
+        btnRClicked();
+        console.log('1- 89');
+    } else if (event.keyCode == 13 || event.keyCode == 32) {
+        resultBtnClicked();
+    } else if (event.keyCode == 39) {
+        btnSkipClicked();
+    } else if (event.keyCode == 8) {
+        event.preventDefault();
+        console.log(answerArr);
+        console.log(userAnswer);
+        if (answerArr.length > 0) {
+            answerArr.pop();
+        }
+        answer.textContent = answerArr.join('');
+    } else if (event.keyCode > 47 && event.keyCode < 58) {
+        console.log('yes: ', event.keyCode);
+        answerArr.push((event.keyCode - 48));
+        var userAnswer = parseInt(answerArr.join(''));
+        answer.textContent = userAnswer;
+    }
+    console.log(event.keyCode);
+}, true);
