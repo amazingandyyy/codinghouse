302ff6c0b5387a537de2e9bde9718861e6a4e9ee
amazingandyyy <qq830214a@gmail.com>
2016-04-16 15:06:08 -0700

    add npm
    add npm

--- temp/intro-to-node/afternoon/app.js
+++ temp/intro-to-node/afternoon/app.js
@@ -8,34 +8,59 @@ console.log('before');
 
 
 
+// fs.readFile('./times.json', (err, data) => {
+//
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     //  RPMSW, CRUD
+//     var timeStamp = `${Date.now()}`;
+//     var arr = JSON.parse(data);
+//     arr.push(timeStamp);
+//     var finalArr = JSON.stringify(arr);
+//
+//     fs.writeFile('./times.json', finalArr, (err) => {
+//         if (err) {
+//             console.log(err);
+//             return; //stop it
+//         }
+//         console.log('arr: ', arr);
+//         console.log('finalArr: ', finalArr);
+//     });
+// });
+
 fs.readFile('./times.json', (err, data) => {
-
-    if (err) {
-        console.log(err);
-        return;
-    }
-    //  RPMSW, CRUD
-    var timeStamp = `\n${Date.now()}`;
+    if (err) return console.log(err);
     var arr = JSON.parse(data);
-    arr.push(timeStamp);
-    var finalArr = JSON.stringify(arr);
-
-    fs.writeFile('./times.json', finalArr, (err) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('arr: ', arr);
-        // console.log('arr.length: ', arr.length;
-        console.log('finalArr: ', finalArr);
-        // console.log('finalArr.length: ', arr.length);
-
-    });
+    arr.push( Date.now() );
+    fs.writeFile('./times.json', JSON.stringify(arr), (err) => {if (err) return console.log(err)});
 });
 
 
-
-
+// //1
+// if (err) {
+//     return console.log(err);
+// }
+// console.log('arr: ', arr);
+//
+// if (err) {
+//     console.log(err);
+//     return; //stop it
+// }
+// // 2
+// console.log('arr: ', arr);
+// if (err) {
+//     console.log(err);
+// }else{
+//   console.log('arr: ', arr);
+// }
+//
+// //3***
+// if (err) return console.log(err);
+// console.log('arr: ', arr);
+//
+// // 1 = 2 = 3;
 
 //
 
