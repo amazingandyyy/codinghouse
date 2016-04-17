5ed05b2c55776c2e6b2e26ec26a15c9788657095
amazingandyyy <qq830214a@gmail.com>
2016-04-17 11:02:27 -0700

    update
    update

--- temp/intro-to-node/afternoon3/app.js
+++ temp/intro-to-node/afternoon3/app.js
@@ -1,60 +1,13 @@
 'use strict';
 
-var fs = require('fs');
-
-asynchronous, the method will not run until the data is been read
-console.log('before');
-fs.readFile('./test.txt', (err, data) => {
-    if (err) {
-      console.log('err: ', err);
-    }
-    console.log(data.toString());
-});
-console.log('after');
+var _ = require('lodash');
+var request = require('request');
 
 
+request('http://pokeapi.co/api/v2/', (error, response, body) => {
+  if(error) return console.log(err);
+    var content = JSON.parse(body);
 
-
-// synchronous, it will wait reading to finish, or it wi lldo nothing
-// console.log('before');
-// var data = fs.readFileSync('./test.txt');
-//
-// var str = data.doIt();
-//
-// function doIt(err, data){
-//     if (err) {
-//       console.log('err: ', err);
-//     }
-//     console.log(data.toString());
-// }
-// console.log('after');
-
-
-
-
-
-
-
-//asynchronous sample: .forEach, .map...
-
-
-
-
-// //case1 synchronous: setTimeout
-// console.log('before');
-// setTimeout(function(){
-//   console.log('done');
-// }, 0);
-// console.log('after');
-// // => return before,after, done;
-//
-//
-// //case 2
-// console.log('before');
-//
-// setTimeout(add());
-// function add(){
-//   console.log('done');
-// };
-// console.log('after');
-// // => return before, done, after;
+    console.log('response: ', response)
+    console.log('body: ', body)
+})
