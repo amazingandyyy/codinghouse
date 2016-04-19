307cf623a0e56a253de293993d699306f93d4c4c
amazingandyyy <qq830214a@gmail.com>
2016-04-19 14:24:26 -0700

    add modemon
    add modemon

---
+++ temp/node-http02/str/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = function(req, res, params) {
+
+    var operation = params.shift().toLowerCase();
+    console.log(operation);
+
+    switch (operation) {
+        case 'wordcount':
+
+            var str = decodeURIComponent(params[0]);
+
+            var count = str.split(' ').length;
+
+            res.write(`${count}\n`)
+            res.end('\n');
+
+            // require('./wordCount')(params, res);
+
+
+            break;
+            // case 'upperCase':
+            //     require('./upperCase')(params, res);
+            //     break;
+        default:
+            res.statusCode = 404;
+            console.error('What do you mena????');
+            res.write('Not Foundddd!');
+            res.end('\n');
+
+    }
+
+
+    res.end('\n');
+
+}
