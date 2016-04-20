d72d4eb0827c1286f945d9cc30163337e81e8c9d
amazingandyyy <qq830214a@gmail.com>
2016-04-20 09:06:16 -0700

    delete 38
    delete 38

--- project/0419customapi/age/index.js
+++ project/0419customapi/age/index.js
@@ -6,8 +6,24 @@ module.exports = function(params, res) {
 
   var time = params.shift();
 console.log(time);
-var year = moment(`${time}`, "YYYYMMDD").fromNow();
+var input = moment(`${time}`, "YYYY/MM/DD").split('/');
+var inputY = input[0];
+var inputM = input[1];
+var inputD = input[2];
+
 var result = year.split(' ').slice(0,1);
+var now = moment().format('YYYY/MM/DD').split('/');
+var nowY = now[0];
+var nowM = now[1];
+var nowD = now[2];
+
+// if(year>nowY){
+//   if(nowM<inputM){
+//     return
+//   }
+// }
+
+console.log('now: ',now);
 result.push('yrs');
 result.push('old');
 var result2 = result.join(' ');
