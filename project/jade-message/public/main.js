5023e0b448776d9ed72ce27c1ecf4a9a8ee18d74
amazingandyyy <qq830214a@gmail.com>
2016-04-21 00:58:35 -0700

    finished

--- project/jade-message/public/main.js
+++ project/jade-message/public/main.js
@@ -8,13 +8,66 @@ function init() {
         $('.jumbotron').css('display', 'block').addClass('animated fadeIn');
         $('.intro').css('display', 'block').addClass('animated bounceIn');
     }, 100)
-    $('.messageDiv').submit($('.messageDiv').submit(messageSubmitted))
+    $('.messageDiv').submit($('.messageDiv').submit(messageSubmitted));
+    $('.themeE').click(themeEClicked)
 }
 
-function messageSubmitted() {
+function themeEClicked() {
+  var theme = $(this).text().toLowerCase();
+  console.log('theme: ',theme);
+  $.get(`theme?${theme}`)
+      .done(function(data) {
+          console.log('data: ', data);
+          // window.open(`http://localhost:8000/post`)
+          // var html = $('html');
+          // html.empty().prepend(data);
+          // window.location.replace(`/theme/${data}`);
+          // var dataArr = data.split(',');
+          // var name = dataArr[0];
+          // var message = dataArr[1];
+          // console.log('name: ', name);
+          // console.log('message: ', message);
+
+
+          // var $newImage = $('.image.template').clone();
+          // $newImage.removeClass('template');
+          // $($newImage).attr('src', `http://gravatar.com/avatar/${data}`).addClass('animated bounceIn');
+          // console.log('$newImage: ', $newImage);
+          // $('.imageContainerR').append($newImage);
+
+      })
+      .fail(function(err) {
+          console.log('gravatarFormSubmitted err: ', err);
+      });
+}
+
+function messageSubmitted(e) {
+    e.preventDefault();
     var name = $('.messageDiv .name').val();
-    var message = $('messageDiv .message').val(),
-        messageUrl = message.split(' ').join('%20');
-    var moment = moment().calendar();
-    $.get(`/fasd/fasd/afsd`)
+    console.log('name: ', name);
+    var message = $('.messageDiv .message').val();
+    console.log('message: ', message);
+    var messageUrl = message.split(' ').join('%20');
+    // console.log('messageUrl: ', messageUrl);
+    // var moment = moment().calendar();
+    $.get(`/send?name=${name}&&message=${message}`)
+        .done(function(data) {
+            console.log('data: ', data);
+            var dataArr = data.split(',');
+            var name = dataArr[0];
+            var message = dataArr[1];
+            console.log('name: ', name);
+            console.log('message: ', message);
+
+
+            // var $newImage = $('.image.template').clone();
+            // $newImage.removeClass('template');
+            // $($newImage).attr('src', `http://gravatar.com/avatar/${data}`).addClass('animated bounceIn');
+            // console.log('$newImage: ', $newImage);
+            // $('.imageContainerR').append($newImage);
+
+        })
+        .fail(function(err) {
+            console.log('gravatarFormSubmitted err: ', err);
+        });
 }
--- project/jade-message/server.js
+++ project/jade-message/server.js
@@ -1,46 +1,86 @@
 'use strict'
 const PORT = process.env.PORT || 8000;
 
-let jade = require('jade');
-let http = require('http');
-let qs = require('qs');
+const jade = require('jade');
+const http = require('http');
+const qs = require('qs');
+
 
 let nodeStatic = require('node-static');
 let file = new nodeStatic.Server('./public');
 
 http.createServer((req, res) => {
+        console.log('req: ', req);
+        console.log('reqUrl: ', req.url);
+        let html;
+        let qsParts = req.url.split('?');
+        console.log('qsParts: ',qsParts);
+        let path = qsParts[0];
+        var theme;
+        if (qsParts[1]) {
+            var query = qs.parse(qsParts[1]);
+            console.log('query');
+            theme = qsParts[1];
+        }
 
-      let html;
-      let qsParts = req.url.split('?');
-      let path = qsParts[0];
-      var theme;
-      if(qsParts[1]){
-        var query = qs.parse(qsParts[1]);
-        theme = query['theme'];
-      }
-
-      console.log('path: ', path);
-      console.log('theme: ', theme);
-      console.log('query: ', query);
+        console.log('path: ', path);
+        console.log('theme: ', theme);
+        console.log('query: ', query);
 
         switch (path) {
-            case '/': {
-                 html = jade.renderFile('./views/index.jade', {
-                  title: "Jade-App",
-                  theme: 'readable'
-                })
-                res.end(html);
-                break;
-              }
-            case '/post': {
-                 html = jade.renderFile('./views/post.jade', {
-                  active: "active",
-                  title: "Chat",
-                  theme: 'readable'
-                })
-                res.end(html);
-                break;
-              }
+            case '/':
+                {
+                    html = jade.renderFile('./views/index.jade', {
+                        title: "Jade-App",
+                        theme: 'readable'
+                    })
+                    res.end(html);
+                    break;
+                }
+            case '/post':
+                {
+                    html = jade.renderFile('./views/post.jade', {
+                        active: "active",
+                        title: "Chat",
+                        theme: 'readable'
+                    })
+                    res.end(html);
+                    break;
+                }
+            case '/theme':
+                {
+                  console.log('themeYYY: ',theme);
+                    html = jade.renderFile('./views/index.jade', {
+                        active: "active",
+                        title: "Chat",
+                        theme: theme
+                    })
+                    res.write(html2);
+                    res.end(html);
+                    break;
+                }
+            case '/send':
+                {
+                  var name = query['name'],
+                      message = query['message'];
+                  console.log('name2: ', name, 'message2: ', message);
+                    html = jade.renderFile('./views/post.jade', {
+                        name: name,
+                        theme: 'readable',
+                        message: message,
+                    })
+                    console.log(html);
+                    res.end(html);
+                    break;
+                }
+                // case '/send': {
+                //   var name = query['name'],
+                //       message = query['message'];
+                //     console.log('name3: ', name, 'message3: ', message);
+                //     res.end(`${name},${message}`);
+                //     // res.end(JSON.stringify({name,message}));
+                //     break;
+                //   }
         }
 
         file.serve(req, res);
